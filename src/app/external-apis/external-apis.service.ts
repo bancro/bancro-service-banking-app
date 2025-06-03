192f2e6ba64d40abb1565d23c80fd7033c25d04f
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2025-06-03 16:17:50 +0100

    Adding custom rate to fixed deposit accouint creation

--- src/app/external-apis/external-apis.service.ts
+++ src/app/external-apis/external-apis.service.ts
@@ -4,7 +4,8 @@ import { HttpClient } from "@angular/common/http";
 
 /** rxjs Imports */
 import { Observable } from "rxjs";
-import { tap } from "rxjs/operators";
+import { tap, catchError } from "rxjs/operators";
+import { of } from "rxjs"; // to handle errors gracefully
 
 /**
  * Service for interacting with external APIs.
@@ -13,12 +14,12 @@ import { tap } from "rxjs/operators";
   providedIn: "root",
 })
 export class ExternalApisService {
-  private readonly baseUrl = "https://bancroapi.streams.com.ng";
+  private  baseUrl = "https://bancroapi.streams.com.ng";
 
   /**
    * @param {HttpClient} http Http Client to send requests.
    */
-  constructor(private readonly http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   /**
    * Downloads a deal certificate by its ID.
@@ -36,29 +37,49 @@ export class ExternalApisService {
           anchor.download = `certificate_${id}.pdf`; // Adjust the filename as needed
           anchor.click();
           window.URL.revokeObjectURL(downloadUrl);
-        })
+        }),
+        catchError((error) => {
+          console.error("Error downloading deal certificate:", error);
+          // Handle error, maybe show a toast or alert to the user
+          return of(null); // Return null or any default value on error
+        }),
       )
       .subscribe();
   }
 
-    /**
+  /**
    * Downloads a repayment schedule by its ID.
    * @param {string} id The ID of the loan to download.
    */
-    downloadRepaymentSchedule(id: string): void {
-      const url = `${this.baseUrl}/Loan/${id}/export-csv`;
-      this.http
-        .get(url, { responseType: "blob" })
-        .pipe(
-          tap((blob) => {
-            const downloadUrl = window.URL.createObjectURL(blob);
-            const anchor = document.createElement("a");
-            anchor.href = downloadUrl;
-            anchor.download = `repayment_schedule_${id}.csv`; // Adjust the filename as needed
-            anchor.click();
-            window.URL.revokeObjectURL(downloadUrl);
-          })
-        )
-        .subscribe();
-    }
+  downloadRepaymentSchedule(id: string): void {
+    const url = `${this.baseUrl}/Loan/${id}/export-csv`;
+    this.http
+      .get(url, { responseType: "blob" })
+      .pipe(
+        tap((blob) => {
+          const downloadUrl = window.URL.createObjectURL(blob);
+          const anchor = document.createElement("a");
+          anchor.href = downloadUrl;
+          anchor.download = `repayment_schedule_${id}.csv`; // Adjust the filename as needed
+          anchor.click();
+          window.URL.revokeObjectURL(downloadUrl);
+        }),
+        catchError((error) => {
+          console.error("Error downloading repayment schedule:", error);
+          // Handle error, maybe show a toast or alert to the user
+          return of(null); // Return null or any default value on error
+        }),
+      )
+      .subscribe();
+  }
+
+  /**
+   * Create new fixed deposit account
+   * @param {any} payload .
+   */
+  addFixedDepositAccount(payload: any): Observable<any> {
+    console.log('Test data')
+    const url = `${this.baseUrl}/FixedDepositAccount/add-account`;
+    return this.http.post(url, payload);
+  }
 }
